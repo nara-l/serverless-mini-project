f0e2f8dffbe73bf1aececdfde0e40c18e617183e
lnara <njielitumbe@gmail.com>
2024-06-27 14:19:43 -0400

    move getpolicy to make it recheable

--- index.js
+++ index.js
@@ -3,38 +3,25 @@
 // The function will return the policy document that will allow or deny access to the client
 
 // Load jwt libraries
-require ('dotenv').config();    
+require('dotenv').config();
 const jwt = require('jsonwebtoken');
 const jwksClient = require('jwks-rsa');
 
 const client = jwksClient({
     jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`
 });
-function getKey(header, callback){
-    client.getSigningKey(header.kid, function(err, key) {
+function getKey(header, callback) {
+    client.getSigningKey(header.kid, function (err, key) {
         const signingKey = key.publicKey || key.rsaPublicKey;
         callback(null, signingKey);
     });
 }
 
-exports.handler = async function(event, context) {
+exports.handler = async function (event, context) {
     // Check if the token is provided
     if (!event.authorizationToken) {
         return context.fail('Unauthorized');
     }
-    const token = event.authorizationToken.replace('Bearer ', '');
-    try {
-        const decode = await jwt.verify(token, getKey, {
-            audience: process.env.AUTH0_AUDIENCE,
-            issuer: `https://${process.env.AUTH0_DOMAIN}/` ,
-            algorithms: ['RS256']
-        });
-
-        return generatePolicy(decode.sub, 'Allow', event.methodArn);
-    } catch(error){
-        return generatePolicy('user', 'Deny', event.methodArn);
-    }
-
     const generatePolicy = (principalId, effect, resource) => {
         const authResponse = {};
         authResponse.principalId = principalId;
@@ -52,5 +39,18 @@ exports.handler = async function(event, context) {
             authResponse.policyDocument = policyDocument;
         }
         return authResponse;
-}
+    }
+    const token = event.authorizationToken.replace('Bearer ', '');
+    try {
+        const decode = await jwt.verify(token, getKey, {
+            audience: process.env.AUTH0_AUDIENCE,
+            issuer: `https://${process.env.AUTH0_DOMAIN}/`,
+            algorithms: ['RS256']
+        });
+
+        return generatePolicy(decode.sub, 'Allow', event.methodArn);
+    } catch (error) {
+        return generatePolicy('user', 'Deny', event.methodArn);
+    }
+
 }
